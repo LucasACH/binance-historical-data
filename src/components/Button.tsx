51e0092e3a2d96cc2dae9f5e4190248a61cc5dfa
Lucas ACH <achaval.lucas@gmail.com>
2022-01-23 23:37:07 -0300

    Add width full

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className='bg-indigo-500 whitespace-nowrap rounded-md shadow-sm h-fit py-2 px-4 text-white disabled:bg-indigo-200'
+      className='bg-indigo-500 w-full whitespace-nowrap rounded-md shadow-sm h-fit py-2 px-4 text-white disabled:bg-indigo-200'
       onClick={onClick}
       disabled={disabled}
     >
